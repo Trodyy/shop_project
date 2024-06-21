fa6a8d317508e7a2fde7b041934ac3caf455008e
mahdi <mahdi.karimi.beiranvand2003@gmail.com>
2024-06-21 11:56:54 +0330

    account_module modified

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #inner_moduld
+    'account_module' ,
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'shop_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +117,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/medias/'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account_module.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'mohamadiyasin499@gmail.com'
+# EMAIL_HOST_PASSWORD = 'bdiilwuwjpuaocha'
+EMAIL_HOST_USER = 'trodykb@gmail.com'
+EMAIL_HOST_PASSWORD = 'akfg abbe juvf dtyu'
+EMAIL_PORT = 587
+#akfg abbe juvf dtyu
